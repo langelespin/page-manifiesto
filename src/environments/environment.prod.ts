9f466b109ec67ce3a101d31aaad1d182ced59b7c
Angel <angel@debian-BULLSEYE-live-builder-AMD64>
2024-01-30 18:05:05 -0600

    cambio enviroment back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  urlBack: 'https://fiap.redalyc.org/back',
-  files: 'https://fiap.redalyc.org/back/siir'
+  // urlBack: 'https://fiap.redalyc.org/back',
+  // files: 'https://fiap.redalyc.org/back/siir'
+
+  urlBack: 'https://ximhai.redalyc.org/back',
+  files: 'https://ximhai.redalyc.org/back/siir'
 };
